b5831438cbe503865d2f95d8c4f37f6242cc410c
oliver <kunhan945@gmail.com>
2022-06-12 10:34:26 +0800

    init

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import { CartIcon } from '../icons';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>Redux Toolkit</h3>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">0</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default NavBar;
